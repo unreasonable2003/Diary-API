45071cc3dc13e3cb8c8819c954e0c66ae9a016b0
Sumit Das <das.sumitsanjay03oo7@gmail.com>
2023-10-04 15:11:03 +0530

    updated user model

--- model/user.go
+++ model/user.go
@@ -1,10 +1,35 @@
 package model
 
-import "gorm.io/gorm"
+import (
+	"diary_api/database"
+	"html"
+	"strings"
+
+	"golang.org/x/crypto/bcrypt"
+	"gorm.io/gorm"
+)
 
 type User struct {
 	gorm.Model
 	Username string `gorm:"size:255;not null;unique" json:"username"`
 	Password string `gorm:"size:255;not null;" json:"-"`
 	Entries  []Entry
+}
+
+func (user *User) Save() (*User, error) {
+	err := database.Database.Create(&user).Error
+	if err != nil {
+		return &User{}, err
+	}
+	return user, nil
+}
+
+func (user *User) BeforeSave(*gorm.DB) error {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+	user.Password = string(hashedPassword)
+	user.Username = html.EscapeString(strings.TrimSpace(user.Username))
+	return nil
 }
