fe70989a4c5c659379299ee84ad4467c43a928ea
Sumit Das <das.sumitsanjay03oo7@gmail.com>
2023-10-05 15:33:13 +0530

    added register controller

---
+++ controller/authentication.go
@@ -0,0 +1,26 @@
+package controller
+
+import (
+	"diary_api/model"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Register(context *gin.Context) {
+	var input model.AuthenticationInput
+	err := context.ShouldBindJSON(&input)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	user := model.User{Username: input.Username, Password: input.Password}
+
+	savedUser, err := user.Save()
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	context.JSON(http.StatusOK, gin.H{"data": savedUser})
+}
